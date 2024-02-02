73ce47e75a80237ec0c68e8d4b465399d186add4
nemorsell <dmitrijborisuk21@gmail.com>
2024-02-02 19:44:04 +0300

    Добавил исходный код проекта

---
+++ main/main.js
@@ -0,0 +1,14 @@
+let menuToggler = document.querySelector('.button')
+let nav = document.getElementById('cont-menu')
+
+if (menuToggler) {
+	menuToggler.addEventListener('click', e => {
+		if (menuToggler.classList.contains('pushed')) {
+			menuToggler.classList.remove('pushed')
+			nav.classList.remove('opened')
+		} else {
+			menuToggler.classList.add('pushed')
+			nav.classList.add('opened')
+		}
+	})
+}
