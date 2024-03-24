6b92d49e7d1e544243353296df181687284b71f6
nemorsell <dmitrijborisuk21@gmail.com>
2024-03-24 18:35:25 +0300

    Улучшл сайт

--- main/main.js
+++ main/main.js
@@ -14,13 +14,23 @@ if (menuToggler) {
 }
 $(document).ready(function(){
 	$(".owl-carousel").owlCarousel(
-	  {
+	{
 		loop:true,
 		margin: 15,
 		mouseDrag:true,
 		dots: 0,
-		
-	  }
+		responsive : {
+			0 : {
+				items: 1,
+			},
+			500 : {
+				items: 2,
+			},
+			800 : {
+				items: 3,
+			},
+		}
+	}
 	);
-  });
+});
 
